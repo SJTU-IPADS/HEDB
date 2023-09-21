8b762d0f7c19583d385546b66ff6052040b882f4
Mingyu Li <lmy2010lmy@gmail.com>
2023-09-21 14:09:29 +0800

    update unit tests

--- tests/unit-test/unit-test.sql
+++ tests/unit-test/unit-test.sql
@@ -8,10 +8,51 @@ SELECT enable_client_mode();
 
 \unset ECHO
 
-SELECT plan(46);
+SELECT plan(48);
 
-SELECT ok(enc_int4_decrypt(enc_int4_encrypt('1')) = '1'::int4, 'enc_int4: encryption/decryption test');
-SELECT ok(enc_float4_decrypt(enc_float4_encrypt('1.1')) = '1.1'::float4, 'enc_float4: encryption/decryption test');
+SELECT ok('2'::enc_int4 + '1'::enc_int4 = '3'::enc_int4, 'enc_int4: operator +');
+SELECT ok('2'::enc_int4 - '1'::enc_int4 = '1'::enc_int4, 'enc_int4: operator -');
+SELECT ok('2'::enc_int4 * '2'::enc_int4 = '4'::enc_int4, 'enc_int4: operator *');
+SELECT ok('6'::enc_int4 / '2'::enc_int4 = '3'::enc_int4, 'enc_int4: operator /');
+SELECT ok('1'::enc_int4 = '1'::enc_int4, 'enc_int4: operator =');
+SELECT ok('0'::enc_int4 != '1'::enc_int4, 'enc_int4: operator !=');
+SELECT ok('0'::enc_int4 <> '1'::enc_int4, 'enc_int4: operator <>');
+SELECT ok('1'::enc_int4 <= '2'::enc_int4, 'enc_int4: operator <=');
+SELECT ok('1'::enc_int4 <= '1'::enc_int4, 'enc_int4: operator <=');
+SELECT ok('3'::enc_int4 >= '2'::enc_int4, 'enc_int4: operator >=');
+SELECT ok('1'::enc_int4 >= '1'::enc_int4, 'enc_int4: operator >=');
+SELECT ok('2'::enc_int4 < '3'::enc_int4, 'enc_int4: operator <');
+SELECT ok('3'::enc_int4 > '2'::enc_int4, 'enc_int4: operator >');
+
+SELECT ok('2.3'::enc_float4 + '1.1'::enc_float4 = '3.4'::enc_float4, 'enc_float4: operator +');
+SELECT ok('2.0'::enc_float4 - '1.1'::enc_float4 = '0.9'::enc_float4, 'enc_float4: operator -');
+SELECT ok('2.0'::enc_float4 * '1.1'::enc_float4 = '2.2'::enc_float4, 'enc_float4: operator *');
+SELECT ok('9.9'::enc_float4 / '3.3'::enc_float4 = '3'::enc_float4, 'enc_float4: operator /');
+SELECT ok('1.1'::enc_float4 = '1.1'::enc_float4, 'enc_float4: operator =');
+SELECT ok('0.2'::enc_float4 != '1.1'::enc_float4, 'enc_float4: operator !=');
+SELECT ok('0.2'::enc_float4 <> '1.1'::enc_float4, 'enc_float4: operator <>');
+SELECT ok('1.1'::enc_float4 <= '2.3'::enc_float4, 'enc_float4: operator <=');
+SELECT ok('1.1'::enc_float4 <= '1.1'::enc_float4, 'enc_float4: operator <=');
+SELECT ok('3.4'::enc_float4 >= '2.3'::enc_float4, 'enc_float4: operator >=');
+SELECT ok('1.1'::enc_float4 >= '1.1'::enc_float4, 'enc_float4: operator >=');
+SELECT ok('2.3'::enc_float4 < '3.4'::enc_float4, 'enc_float4: operator <');
+SELECT ok('3.4'::enc_float4 > '2.3'::enc_float4, 'enc_float4: operator >');
+
+SELECT ok('11/11/12'::enc_timestamp = '11/11/12'::enc_timestamp, 'enc_timestamp: operator =');
+SELECT ok('11/11/12 00:00:01'::enc_timestamp != '11/11/12 00:00:02'::enc_timestamp, 'enc_timestamp: operator !=');
+SELECT ok('11/11/12 00:00:01'::enc_timestamp <> '11/11/12 00:00:02'::enc_timestamp, 'enc_timestamp: operator <>');
+SELECT ok('11/11/12 00:00:01'::enc_timestamp <= '11/11/12 00:00:01'::enc_timestamp, 'enc_timestamp: operator <=');
+SELECT ok('11/11/12 00:00:01'::enc_timestamp <= '11/11/12 00:00:03'::enc_timestamp, 'enc_timestamp: operator <=');
+SELECT ok('11/11/12 00:00:01'::enc_timestamp >= '11/11/12 00:00:01'::enc_timestamp, 'enc_timestamp: operator >=');
+SELECT ok('11/11/12 00:00:02'::enc_timestamp >= '11/11/12 00:00:01'::enc_timestamp, 'enc_timestamp: operator >=');
+SELECT ok('11/11/12 00:00:01'::enc_timestamp < '11/11/12 00:00:03'::enc_timestamp, 'enc_timestamp: operator <');
+SELECT ok('11/11/12 00:00:03'::enc_timestamp > '11/11/12 00:00:01'::enc_timestamp, 'enc_timestamp: operator >');
+
+SELECT ok('test1'::enc_text = 'test1'::enc_text, 'enc_text: operator =');
+SELECT ok('test1'::enc_text != 'test2'::enc_text, 'enc_text: operator !=');
+SELECT ok('test1'::enc_text <> 'test2'::enc_text, 'enc_text: operator <>');
+SELECT ok('hello'::enc_text || 'world'::enc_text = 'helloworld'::enc_text, 'enc_text: operator ||');
+SELECT ok('edb'::enc_text = substring('hedb'::enc_text, '2'::enc_int4, '3'::enc_int4), 'enc_text: operator substring');
 
 DROP TABLE IF EXISTS test_table;
 CREATE TABLE test_table (id int, num_i enc_int4, num_f enc_float4, str enc_text, time enc_timestamp);
@@ -26,7 +67,16 @@ SELECT results_eq(
     $$VALUES ('9'::enc_int4)$$,
     'enc_int4: SUM function '
 );
-
+SELECT results_eq(
+   'SELECT AVG(num_i) FROM test_table',
+    $$VALUES ('2'::enc_int4)$$,
+    'enc_int4: AVG function (with rounding)'
+);
+SELECT results_eq(
+   'SELECT MAX(num_i) FROM test_table',
+    $$VALUES ('3'::enc_int4)$$,
+    'enc_int4: MAX function '
+);
 SELECT results_eq(
    'SELECT MIN(num_i) FROM test_table',
     $$VALUES ('1'::enc_int4)$$,
@@ -34,71 +84,25 @@ SELECT results_eq(
 );
 
 SELECT results_eq(
-   'SELECT MAX(num_i) FROM test_table',
-    $$VALUES ('3'::enc_int4)$$,
-    'enc_int4: MAX function '
-);
-
-SELECT results_eq(
-   'SELECT AVG(num_i) FROM test_table',
-    $$VALUES ('2'::enc_int4)$$,
-    'enc_int4: AVG function (with rounding)'
-);
-
-SELECT results_eq(
    'SELECT SUM(num_f) FROM test_table',
     $$VALUES ('9.4'::enc_float4)$$,
     'enc_float4: SUM function'
 );
-
 SELECT results_eq(
    'SELECT AVG(num_f) FROM test_table',
     $$VALUES ('2.35'::enc_float4)$$,
     'enc_float4: AVG function'
 );
-
-SELECT ok('1'::enc_int4 = '1'::enc_int4, 'enc_int4: inequality test, operator =');
-SELECT ok('0'::enc_int4 != '1'::enc_int4, 'enc_int4: inequality test, operator !=');
-SELECT ok('0'::enc_int4 <> '1'::enc_int4, 'enc_int4: inequality test, operator <>');
-SELECT ok('1'::enc_int4 <= '2'::enc_int4, 'enc_int4: inequality test, operator <=');
-SELECT ok('1'::enc_int4 <= '1'::enc_int4, 'enc_int4: inequality test, operator <=');
-SELECT ok('3'::enc_int4 >= '2'::enc_int4, 'enc_int4: inequality test, operator >=');
-SELECT ok('1'::enc_int4 >= '1'::enc_int4, 'enc_int4: inequality test, operator >=');
-SELECT ok('2'::enc_int4 < '3'::enc_int4, 'enc_int4: inequality test, operator <');
-SELECT ok('3'::enc_int4 > '2'::enc_int4, 'enc_int4: inequality test, operator >');
-
-SELECT ok('2'::enc_int4 + '1'::enc_int4 = '3'::enc_int4, 'enc_int4: operator +');
-SELECT ok('2'::enc_int4 - '1'::enc_int4 = '1'::enc_int4, 'enc_int4: operator -');
-SELECT ok('2'::enc_int4 * '2'::enc_int4 = '4'::enc_int4, 'enc_int4: operator *');
-SELECT ok('6'::enc_int4 / '2'::enc_int4 = '3'::enc_int4, 'enc_int4: operator /');
-
-SELECT ok('1.1'::enc_float4 = '1.1'::enc_float4, 'enc_float4: inequality test, operator =');
-SELECT ok('0.2'::enc_float4 != '1.1'::enc_float4, 'enc_float4: inequality test, operator !=');
-SELECT ok('0.2'::enc_float4 <> '1.1'::enc_float4, 'enc_float4: inequality test, operator <>');
-SELECT ok('1.1'::enc_float4 <= '2.3'::enc_float4, 'enc_float4: inequality test, operator <=');
-SELECT ok('1.1'::enc_float4 <= '1.1'::enc_float4, 'enc_float4: inequality test, operator <=');
-SELECT ok('3.4'::enc_float4 >= '2.3'::enc_float4, 'enc_float4: inequality test, operator >=');
-SELECT ok('1.1'::enc_float4 >= '1.1'::enc_float4, 'enc_float4: inequality test, operator >=');
-SELECT ok('2.3'::enc_float4 < '3.4'::enc_float4, 'enc_float4: inequality test, operator <');
-SELECT ok('3.4'::enc_float4 > '2.3'::enc_float4, 'enc_float4: inequality test, operator >');
-SELECT ok('2.3'::enc_float4 + '1.1'::enc_float4 = '3.4'::enc_float4, 'enc_float4: operator +');
-SELECT ok('9.9'::enc_float4 / '3.3'::enc_float4 = '3'::enc_float4, 'enc_float4: operator /');
-
-SELECT ok('11/11/12'::enc_timestamp = '11/11/12'::enc_timestamp, 'enc_timestamp: inequality test, operator =');
-SELECT ok('11/11/12 00:00:01'::enc_timestamp != '11/11/12 00:00:02'::enc_timestamp, 'enc_timestamp: inequality test, operator !=');
-SELECT ok('11/11/12 00:00:01'::enc_timestamp <> '11/11/12 00:00:02'::enc_timestamp, 'enc_timestamp: inequality test, operator <>');
-SELECT ok('11/11/12 00:00:01'::enc_timestamp <= '11/11/12 00:00:01'::enc_timestamp, 'enc_timestamp: inequality test, operator <=');
-SELECT ok('11/11/12 00:00:01'::enc_timestamp <= '11/11/12 00:00:03'::enc_timestamp, 'enc_timestamp: inequality test, operator <=');
-SELECT ok('11/11/12 00:00:01'::enc_timestamp >= '11/11/12 00:00:01'::enc_timestamp, 'enc_timestamp: inequality test, operator >=');
-SELECT ok('11/11/12 00:00:02'::enc_timestamp >= '11/11/12 00:00:01'::enc_timestamp, 'enc_timestamp: inequality test, operator >=');
-SELECT ok('11/11/12 00:00:01'::enc_timestamp < '11/11/12 00:00:03'::enc_timestamp, 'enc_timestamp: inequality test, operator <');
-SELECT ok('11/11/12 00:00:03'::enc_timestamp > '11/11/12 00:00:01'::enc_timestamp, 'enc_timestamp: inequality test, operator >');
-
-SELECT ok('test1'::enc_text = 'test1'::enc_text, 'enc_text: inequality test, operator =');
-SELECT ok('test1'::enc_text != 'test2'::enc_text, 'enc_text: inequality test, operator !=');
-SELECT ok('test1'::enc_text <> 'test2'::enc_text, 'enc_text: inequality test, operator <>');
-SELECT ok('hello'::enc_text || 'world'::enc_text = 'helloworld'::enc_text, 'enc_text: operator ||');
-SELECT ok('edb'::enc_text = substring('hedb'::enc_text, '2'::enc_int4, '3'::enc_int4), 'enc_text: operator substring');
+SELECT results_eq(
+   'SELECT MAX(num_f) FROM test_table',
+    $$VALUES ('3.1'::enc_float4)$$,
+    'enc_float4: MAX function'
+);
+SELECT results_eq(
+   'SELECT MIN(num_f) FROM test_table',
+    $$VALUES ('1.1'::enc_float4)$$,
+    'enc_float4: MIN function'
+);
 
 SELECT * FROM finish();
 DROP TABLE IF EXISTS test_table;
